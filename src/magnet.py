d1364f5585ad2c48b7234f13093967e8afd762ec
Jun Wu <quark@lihdd.net>
2025-12-09 23:47:18 -0800

    magnet: support 2x10x20 and round hole

--- src/magnet.py
+++ src/magnet.py
@@ -2,7 +2,10 @@ from cqutils import *
 from functools import partial
 
 
-def _magnet(m_w, m_h, m_t, hole_depth=1, c1a=0.4, c1b=1.0, c2=0.6, thick=None):
+@cq_cache
+def _magnet(
+    m_w, m_h, m_t, hole_depth=1, c1a=0.4, c1b=1.0, c2=0.6, thick=None, round_hole=None
+):
     obj = b = (
         W()
         .box(m_w, m_t, m_h)
@@ -16,17 +19,34 @@ def _magnet(m_w, m_h, m_t, hole_depth=1, c1a=0.4, c1b=1.0, c2=0.6, thick=None):
         assert hole_depth > 0
     if hole_depth:
         # b1 is to punch a "hole" to make it easier to remove the magnet later
-        b1 = W().box(m_w, m_w, hole_depth).align(b, ":>Z")
+        if round_hole is None:
+            b1 = W().box(m_w, m_w, hole_depth).align(b, ":>Z")
+        else:
+            if round_hole is True:
+                r = m_w / 3
+            else:
+                r = round_hole / 2
+            h1 = W().cylinder(hole_depth, r)
+            b1 = h1.align(b, ":>Z")
         # asymmetrical - the shorter side should be installed first
         b1 = b1.translate((0, (int(c1b > c1a) - 0.5) * (m_t - m_w) / 4, 0))
         obj = obj.union(b1)
     return obj
 
 
-magnet2510 = partial(_magnet, m_w=5.0, m_h=2.2, m_t=10.1)
+magnet_2_5_10 = magnet2510 = partial(_magnet, m_w=5.0, m_h=2.2, m_t=10.1)
 magnet2510.__doc__ = """slot for magnet: 10 x 5 x 2mm (useful for cut)"""
 
-magnet31060 = partial(_magnet, m_w=9.9, m_h=3.1, m_t=59.3, c1b=3.0, c1a=0.8)
+# actual: 1.7mm x 9.0mm x 19mm
+# not yet verified
+magnet_2_10_20 = magnet21020 = partial(
+    _magnet, m_w=9.4, m_h=2.0, m_t=19.2, c1a=0.6, c1b=2.0, round_hole=True
+)
+magnet2510.__doc__ = """slot for magnet: 20 x 10 x 2mm (useful for cut)"""
+
+magnet_3_10_60 = magnet31060 = partial(
+    _magnet, m_w=9.9, m_h=3.1, m_t=59.3, c1b=3.0, c1a=0.8
+)
 magnet31060.__doc__ = """slot for magnet: 60 x 10 x 3mm (useful for cut)"""
 
 
