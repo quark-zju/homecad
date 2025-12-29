bae94fff60b50535051823c3c54a537145eaae5e
Jun Wu <quark@lihdd.net>
2025-12-28 19:04:23 -0800

    shower_holder: minor tweaks

--- src/misc/shower_holder.py
+++ src/misc/shower_holder.py
@@ -5,10 +5,13 @@ WIDTH = 63 / 4
 HEIGHT = 46
 
 
-def render(r1=22 / 2, r2=24 / 2, s1=16, h1=30):
+def render(r1=23 / 2, r2=25 / 2, s1=16, h1=35):
     p1 = import_part("command_strip_plate.py", "flat2-female").rotate_axis("X", 90)
+    p1a = p1.surface_grow(">Z", 6).edges("|Y and >Z").fillet(5)
+    p1b = p1.surface_grow("<Z", 20)
+    p1 = p1.union(p1a).union(p1b)
     obj = p1
-    angle = 5
+    angle = 3
     h1 = h1 or p1.measure("Z")
     l1 = (
         W().circle(r1).workplane(offset=h1).circle(r2).loft(combine=True)
@@ -42,7 +45,7 @@ def render(r1=22 / 2, r2=24 / 2, s1=16, h1=30):
     l3 = l3.align(p1, ":>Y <Z")
     b1 = W().box(s1, w1 + r1, h2)
     l3 = l3.cut(b1.align(l3, ">Y >Z -X"))
-    l3 = l3.edges("not <Y or <Z").fillet(1.6)
+    l3 = l3.edges("not <Y or <Z").fillet(1.6)  # slow, comment out when iterating
     obj = obj.union(l3)
     return obj
 
