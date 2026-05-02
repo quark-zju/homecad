e64f5bbd0df25cd721f688ec8f6c6e300489fe8d
Jun Wu <quark@lihdd.net>
2026-05-02 15:44:36 -0700

    lap_desk_stablizer: update

--- src/misc/lap_desk_leg_stabilizer.py
+++ src/misc/lap_desk_leg_stabilizer.py
@@ -6,14 +6,14 @@ from cqutils import W, trapezoid, union_all
 bar_width = 20.2
 bar_thick = 3.1
 
-l1 = 45
+l1 = 45  # bottom cover length
 l2 = 30  # length for the round corner
-l3 = 12
+l3 = 30  # top cover length
 pad = 2
 
 rd = 8.7
 # r + rd == sqrt(2) * r
-r = 2.414 * rd
+r = 2.414 * rd - 1
 
 
 def render():
@@ -25,35 +25,32 @@ def render():
     b2r = b2c.rotate_axis("Z", 180).align(b1, ">X :>Z")
     o1 = union_all([b1, b2l, b2r])
 
-    b3 = W().box(bar_width, l2, pad).align(b1, ":<Y")
+    b2ls = b2.align(b1, "<X :>Z")
+    b2rs = b2ls.align(b1, ">X :>Z")
+    o1s = union_all([b1, b2ls, b2rs])
+
+    b3 = W().box(bar_width + pad * 2.4, l2, pad).align(b1, ":<Y")
+    t2h = r + pad + l3
     t2 = (
-        trapezoid(0, l2 + l3, bar_width, dx2=0, degree=30)
+        trapezoid(0, t2h, bar_width + pad * 2.4, dx2=0, degree=30)
         .rotate_axis("X", 90)
         .rotate_axis("Z", 90)
-        .align(b3, "<X <Z :<Y")
+        .align(b3, "-X <Z :<Y")
         .edges("not >Y")
         .edges("not <Z")
         .fillet(1)
     )
-    t2c1 = trapezoid(0, pad, l3 + pad * 2, dx2=0)
-    t2cr = t2c1.align(t2, ">X >Y >Z")
-    t2cl = t2cr.rotate_axis("Y", 180).align(t2, "<X >Y >Z")
-    t2 = t2.cut(t2cr).cut(t2cl)
-
-    b4 = W().box(bar_width + pad * 2, pad, l3)
-    b4l = W().box(pad, bar_thick + pad, l3).align(b4, "<X :<Y")
-    b4r = b4l.align(b4, ">X")
-    t4 = trapezoid(0, pad, l3, dx2=0)
-    t4r = t4.align(b4r, ":<X >Z <Y")
-    t4l = t4.rotate_axis("Y", 180).align(b4l, ":>X >Z <Y")
-    o4 = union_all([b4, b4l, b4r, t4r, t4l])
-
-    o4.export("vertical-cap", print_from_face=">Y")
-    o4 = o4.align(t2, ">Z >Y", dy=pad + bar_thick)
+    # t2c1 = trapezoid(0, pad, l3 + pad * 2, dx2=0)
+    # t2cr = t2c1.align(t2, ">X >Y >Z")
+    # t2cl = t2cr.rotate_axis("Y", 180).align(t2, "<X >Y >Z")
+    o1r = o1.rotate_axis("X", -90).align(t2, ">Z >Y", dy=pad + bar_thick)
+    o1rs = o1s.rotate_axis("X", -90).align(t2, "<Z >Y", dy=pad + bar_thick)
+    o1rs = o1rs.cut(o1rs.align(dz=r + pad))
+    t2 = t2.union(o1r).union(o1rs)
 
     c1 = (
         W()
-        .cylinder(bar_width, r, angle=90)
+        .cylinder(bar_width + pad * 2.4, r, angle=90)
         .rotate_axis("Y", 90)
         .rotate_axis("Z", 180)
         .align(b3, ":>Z <Y")
@@ -64,9 +61,13 @@ def render():
 
     b10 = W().box(bar_width + pad * 2.4, l1, t1.measure("Z")).align(b2l, "<X <Y >Z")
     o3 = b10.cut(o1).cut(o1.align(dx=-0.1))
-    o3.export("cap")
+    o3.export("cap1")
 
-    return union_all([o2, o3.align(dy=10), o4.align(dz=5)])
+    o4 = o3.rotate_axis("X", -90).align(o1r, ">Z >Y")
+    o4 = o4.cut(o4.align(dz=-l3))
+    o4.export("cap2", print_from_face="<Y")
+
+    return union_all([o2, o3.align(dy=10), o4.align(dz=10)])
 
 
 render().show()
