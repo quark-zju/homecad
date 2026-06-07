54237bc0e85c3e6c6e20efd53bf507e0e14925b9
Jun Wu <quark@lihdd.net>
2026-06-07 15:14:14 -0700

    Update drain_hair_catcher2.py

--- src/misc/drain_hair_catcher2.py
+++ src/misc/drain_hair_catcher2.py
@@ -1,10 +1,11 @@
 from b4dcad import cylinder
 
 
-def get_obj(r1=70 / 2, r2=51.2 / 2, w1=17, n=20, slot_width=2, thick=1.2):
+def get_obj(r1=70 / 2, r2=51.2 / 2, w1=17, n=20, slot_width=2.4, thick=1.2):
     c1 = cylinder(h=thick, r=r1)
     c2 = cylinder(h=thick, r=r2)
-    obj = c1 - c2
+    c3 = c1 - c2
+    obj = c3
     b0 = cylinder(h=w1, d=slot_width).align_to(c1, ">X")
     b1 = cylinder(h=w1, d=slot_width).align_to(c2, ":>X")
     b2 = b1.move(x=(r1 - r2 - slot_width) / 2.0).rotate(z=180.0 / n)
@@ -12,6 +13,7 @@ def get_obj(r1=70 / 2, r2=51.2 / 2, w1=17, n=20, slot_width=2, thick=1.2):
         angle = 360.0 / n * i
         bi = b0.rotate(z=angle) + b1.rotate(z=angle) + b2.rotate(z=angle)
         obj += bi
+    obj = obj + c3.align_to(obj, ">Z")
     return obj
 
 
