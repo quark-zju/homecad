f85a496f5dc4f0c1d51705b0f46ab2b881e2091b
Jun Wu <quark@lihdd.net>
2026-06-12 01:43:50 -0700

    Update connector2.py

--- src/misc/connector2.py
+++ src/misc/connector2.py
@@ -1,16 +1,19 @@
-from b4dcad import cube, cylinder
+from b4dcad import conic, cube, cylinder
 
 
-def get_obj(r1=6.0, thick=5.0, hook_len=40.0, tall_len=150.0):
+def semicircular(r1, thick):
     c1 = cylinder(h=thick, r=r1)
     c2 = cylinder(h=thick, r=r1 + thick)
     b1 = cube(r1 + thick, (r1 + thick) * 2, thick).align_to(c2, ">X >Y")
-    c3 = c2 - c1 - b1
-    b2 = cube(hook_len, thick, thick).align_to(c3, ":>X <Y")
-    b3 = cube(tall_len, thick, thick).align_to(c3, ":>X >Y")
-    c4 = c3.rotate(z=180, x=90).align_to(b3, ":>X >Y <Z")
-    b4 = b2.align_to(c4, ">Z >Y :<X")
-    obj = c3 + b2 + b3 + c4 + b4
+    return c2 - c1 - b1
+
+
+def get_obj(r1=6.1 / 2, r2=9.8 / 2, thick=5.0, hook_len=40.0, tall_len=210.0):
+    c1 = semicircular(r1, thick)
+    b2 = cube(hook_len, thick, thick).align_to(c1, ":>X <Y")
+    b3 = cube(tall_len, thick, thick).align_to(c1, ":>X >Y")
+    c2 = conic(r1=thick / 2, r2=r2, h=2.9 + 1).align_to(b3, ":>Z -Y >X")
+    obj = c1 + b2 + b3 + c2
     return obj
 
 
